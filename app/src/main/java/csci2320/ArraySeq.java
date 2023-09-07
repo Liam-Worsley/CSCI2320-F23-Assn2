ca4cb659496ae1e671f8016f2f326a088aa6766e
Liam Worsley <lworsley@trinity.edu>
2023-09-06 20:24:41 -0500

    get set add insert remove size

--- app/src/main/java/csci2320/ArraySeq.java
+++ app/src/main/java/csci2320/ArraySeq.java
@@ -7,7 +7,8 @@ import java.util.function.Function;
 
 public class ArraySeq<E> implements Seq<E> {
   // Put your private data here
-
+  private int numElems = 0;
+  @SuppressWarnings("unchecked") private E[] data = (E[]) new Object[10];
   // I'm giving you two static factories and an equals method to make testing easier.
   @SuppressWarnings("unchecked")
   public static <E> ArraySeq<E> of(E... elems) {
@@ -32,6 +33,14 @@ public class ArraySeq<E> implements Seq<E> {
     return true;
   }
 
+
+  private E[] doubleSize(){
+   @SuppressWarnings("unchecked") E[] newdata = (E[]) new Object[numElems*2];
+    for(int i=0;i<numElems;i++){
+      newdata[i] = data[i];
+    }
+   return newdata;
+  }
   // Implement the methods below and test them.
   @Override
   public Iterator<E> iterator() {
@@ -47,38 +56,61 @@ public class ArraySeq<E> implements Seq<E> {
 
   @Override
   public E get(int index) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'get'");
+    if (index < 0 || index >= numElems){
+    throw new IndexOutOfBoundsException("Get out of bounds at " + index + " of " + numElems);
+    }
+  return data[index];
   }
 
   @Override
   public void set(int index, E elem) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'set'");
+    if (index < 0 || index > numElems){
+    throw new IndexOutOfBoundsException("Insert out of bounds at " + index + " of " + numElems);
+    }
+    data[index] = elem;
   }
 
   @Override
   public void add(E elem) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'add'");
+    if (numElems >= data.length) {
+    data = doubleSize();
+  }
+  data[numElems] = elem;
+  numElems++;
   }
 
   @Override
   public void insert(int index, E elem) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    if (index < 0 || index > numElems){
+    throw new IndexOutOfBoundsException("Insert out of bounds at " + index + " of " + numElems);
+    }
+  if (numElems >= data.length) {
+    data = doubleSize();
+  }
+  for (int i = numElems; i > index; --i) {
+    data[i] = data[i-1];
+  }
+  data[index] = elem;
+  numElems++;
   }
 
   @Override
   public E remove(int index) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'remove'");
+      if (index < 0 || index > numElems){
+    throw new IndexOutOfBoundsException("Insert out of bounds at " + index + " of " + numElems);
+    }
+    E tmp = data[index];
+    for(int i=index; i<numElems-1;i++){
+      data[i] = data[i+1];
+    }
+    data[numElems-1] = null;
+    numElems--;
+    return tmp;
   }
 
   @Override
   public int size() {
-    // TODO Auto-generated method stub
-    return 0; // Temporary hard coded to make the one test pass.
+    return numElems; 
   }
 
   @Override
--- app/src/test/java/csci2320/ArraySeqTest.java
+++ app/src/test/java/csci2320/ArraySeqTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ArraySeqTest {
-  private ArraySeq<Integer> seq;
+  private ArraySeq<Object> seq;
 
   @BeforeEach void makeArraySeq() {
     seq = new ArraySeq<>();
@@ -14,5 +14,67 @@ public class ArraySeqTest {
 
   @Test void emptyOnCreate() {
     assertEquals(0, seq.size());
+  }
+
+  @Test void sizeCheck(){
+    seq.add(4);
+    seq.add(5);
+    seq.add(9);
+    assertEquals(3,seq.size());
+  }
+
+  @Test void addingObjects(){
+    seq.add(5);
+    seq.add(6);
+    assertEquals(2,seq.size());
+  }
+  @Test void addingManyThings(){
+    for(int i = 0; i < 5000; i++){
+      seq.add(i);
+    }
+    assertEquals(5000,seq.size());
+  }
+
+  @Test void addingMultipleTypes(){
+    seq.add(5);
+    seq.add("HI");
+    seq.add(40.243);
+    assertEquals(3, seq.size());
+  }
+
+  @Test void gettingObjects(){
+    seq.add(5);
+    seq.add(7);
+    seq.add(20);
+    assertEquals(5,seq.get(0));
+    assertEquals(7,seq.get(1));
+    assertEquals(20,seq.get(2));
+  }
+
+  @Test void settingObjects(){
+    seq.add(5);
+    seq.add(7);
+    seq.add(20);
+    seq.set(1,6);
+    assertEquals(6,seq.get(1));
+  }
+
+    @Test void insertingObjects(){
+    seq.add(5);
+    seq.add(7);
+    seq.add(20);
+    seq.insert(1,6);
+    assertEquals(6,seq.get(1));
+    assertEquals(7,seq.get(2));
+    assertEquals(20,seq.get(3));
+  }
+
+    @Test void removingObjects(){
+    seq.add(5);
+    seq.add(7);
+    seq.add(20);
+    seq.remove(1);
+    assertEquals(20,seq.get(1));
+    assertEquals(2,seq.size());
   }
 }
